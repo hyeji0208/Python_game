dcfbf0ed4da1bf6eef8b03259ff011ede7de146c
hyeji0208 <mos137137@gmail.com>
2024-12-04 23:22:50 +0900

    modified:   graphic/explainbutton.png 	modified:   main_screen.py

--- main_screen.py
+++ main_screen.py
@@ -14,17 +14,40 @@ main_screen = Entity(
 # bgm
 main_bgm = Audio(sound_file_name='bgm/게임 메인.mp3', volume=1, pitch=1, loop=True, autoplay=True, auto_destroy=True)
 
-# button
+# 시작버튼
 start_button = Button(
     model='quad',
     texture = 'graphic/startbutton.png',
     scale=(0.416, 0.13),
     position=(0.34, -0.334),
     color= color.white,
-    highlight_color = color.white,
+    highlight_color = color.gray,
     pressed_color = color.white
     )
 
+# 설명버튼
+explain_button = Button(
+    model='quad',
+    texture = 'graphic/explainbutton.png',
+    scale=(0.416, 0.13),
+    position=(-0.33, -0.334),
+    color= color.white,
+    highlight_color = color.gray,
+    pressed_color = color.white
+    )
+
+# 게임설명창
+explain_screen = Entity(
+    model='quad',
+    texture='graphic/explain.png',              
+    scale=(14.6, 8.2),
+    enabled=False             
+)
+
+def explain():
+    explain_screen.enable()
+
+explain_button.on_click = explain
 
 # start_button.on_click = start_game
 
